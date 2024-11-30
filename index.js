461344cb3b728c4b4f3570b6accb40bbf867dc2f
chris <chris@Chris-MacBook-Air.local>
2024-11-30 10:20:51 -0600

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-alert("hello world");
+//alert("hello world");
+
+var element = document.getElementById('welcome');
+var text = element.innerText.substring(9);
+alert("Do you want to play a game, " + text + "?");
+
+//var element = document.getElementById('sv_footer');
+//sv_footer.remove();
